6df2e145a2114334895fd527431dce47438ed0d9
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-03-13 03:51:59 +0900

    nexflix
    ddd

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,63 @@
 import axios from '../api/axios';
-import requests from '../api/requests';
-import React, { useState,useEffect } from 'react'
+import requests from "../api/requests";
+import React, { useEffect,useState } from 'react';
+import "./Banner.css";
 
-export default function () {
-    const[movie,setMovie] = useState([]);
+export default function Banner() {
+  const [movie,setMovie]=useState([]);
+  const [isClicked,setIsClicked]=useState(false);
+  useEffect(()=>{
+    fetchData();
+  },[]);
 
-    useEffect(()=>{
-        fetchData();
-    },[]);
+  const fetchData= async() => {
+    // 현재 상영중인 영화 정보를 가져오기
+    const request = await axios.get(requests.fetchNowPlaying);
 
-    const fetchData=async()=>{
-        //현재 상영중인 영화정보를 가져오기
-        const request = axios.get(requests.fetchNowPlaying);
-        console.log(request);
-    }
+    const movieId = request.data.results[
+      Math.floor(Math.random()*request.data.results.length)
+    ].id;
+
+    //특정 영화의 더 상세한 정보를 가져오기(비디오 정보 포함)
+
+    const {data:movieDetail} = await axios.get(`movie/${movieId}`,{
+      params:{append_to_response:"videos"},
+    });
+    setMovie(movieDetail);
+  };
+
+  const truncate = (str,n) =>{
+    return str?.length>n?str.substr(0,n-1)+"...":str;
+  };
   return (
-    <div></div>
+    <header
+      className='banner'
+      style={{
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition:"top center",
+        backgroundSize:"cover"
+      }}
+    >
+      <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie.overview,100)}
+          </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+
+    </header>
   )
 }
